0b6de5a9b6e6daf8198dd2c61b579165d68bc7fb
BertrandHustle <MFOrangeSlice@gmail.com>
2019-04-10 18:34:06 -0400

    initial commit

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+type Property struct {
+	baseCost   int
+	cost       int
+	group      string
+	investment int
+	owner			  
+
+	// calculate total cost of property
+	func(calcCost)
+}
